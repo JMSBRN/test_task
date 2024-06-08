cc4fb6582152a752472823a3232d914d925d1482
JMSBRN <zakhavai@gmail.com>
2024-06-08 15:14:50 +0300

    feat: created modal menu with burger button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-   
+    var menuButton = document.querySelector('.header__menu-button');
+    var modalMenu = document.querySelector('.modal-menu');
+    var closeButton = document.querySelector('.modal-menu__close-btn');
+
+    function showModal() {
+        modalMenu.classList.remove('hidden-element');
+    }
+
+    function hideModal() {
+        modalMenu.classList.add('hidden-element');
+    }
+
+    menuButton.addEventListener('click', showModal);
+    closeButton.addEventListener('click', hideModal);
+
+    window.addEventListener('resize', hideModal);
+    window.addEventListener('scroll', hideModal);
 });
